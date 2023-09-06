45052f088068214a2a39ca693841777e5d430127
Emmaibe <emmanuelikechukwu14@gmail.com>
2023-09-07 00:37:53 +0100

    completed test for add to stock

--- src/test/java/com/lordibe/store/services/service/CustomerServicesTest.java
+++ src/test/java/com/lordibe/store/services/service/CustomerServicesTest.java
@@ -2,7 +2,9 @@ package com.lordibe.store.services.service;
 
 import com.lordibe.store.abstracts.Check;
 import com.lordibe.store.abstracts.enums.PRODUCT_CATEGORY;
+import com.lordibe.store.model.customer.Customer;
 import com.lordibe.store.model.product.Products;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -11,19 +13,38 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerServicesTest {
+    CustomerServices customerTest = new CustomerServices("Obinna", "08035462791");
+    ManagerServices ms = new ManagerServices();
 
-//    Map<String, Integer> cart = customerTest.getCartContent();
+    @BeforeEach
+    void setUp() {
+        ms.updateStock();
 
-    @Test
-    void addToCart() {
-        CustomerServices customerTest = new CustomerServices("Obinna", "08035462791");
-        Map<String, Integer> cart = customerTest.getCartContent();
         customerTest.addToCart("bread", 6);
         customerTest.addToCart("tea", 2);
         customerTest.addToCart("gin", 3);
+    }
 
-        // Assuming addToCart returns a specific message
+    @Test
+    void addToCartTest() {
+        String outcome = customerTest.addToCart("vodka", 4);
 
-        assertTrue(cart.keySet().contains("tea"));
+        assertEquals("added", outcome);
+        assertTrue(customerTest.getCartContent().containsKey("tea"));
+    }
+
+    @Test
+    void mergedProductQuantityTest() {
+        String outcome = customerTest.addToCart("bread", 4);
+
+        assertEquals("merged", outcome);
+        assertEquals(10, customerTest.getCartContent().get("bread"));
+    }
+
+    @Test
+    void notInStock() {
+        String outcome = customerTest.addToCart("notInCart", 4);
+
+        assertEquals("failed", outcome);
     }
 }
